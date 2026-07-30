f324f95040377c003ca6f08688efb95edd3d1288
= <=>
2026-07-30 11:10:26 +0200

    Backend init

---
+++ backend/ImigramAPI/ImigramAPI/Database/MongoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+
+namespace ImigramAPI.Database
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+        
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["MongoDb:ConnectionString"]);
+
+            _database = client.GetDatabase(configuration["MongoDb:Database"]);
+        }
+
+        public IMongoDatabase Database => _database;
+    }
+}
